c98bb704ac09e0135a9462d54321caa9919d5b26
eatmoarrice <63140320+eatmoarrice@users.noreply.github.com>
2020-08-23 16:59:52 +0700

    remind everyone

--- index.js
+++ index.js
@@ -267,12 +267,12 @@ bot.on('message', async (msg) => {
 
 bot.once('ready', () => {
 	cron.schedule(
-		// '0-59 * * * *',
+		// min hour day month dayOfWeek
 		'0 11 1-31 1-12 0,1,3,4,5,6',
 		async () => {
 			try {
 				const channel = await bot.channels.fetch('698917300040106084');
-				if (channel) channel.send('@here 3 hours until GB ends!! Try your best!');
+				if (channel) channel.send('@everyone 3 hours until GB ends!! Try your best!');
 				if (channel) channel.send('<:mongoose:740392968015577121>');
 				else console.log('Could not find channel');
 			} catch (err) {
