2c131f33ebb357b172f728f5cdafb6fb9a2a13fe
unknown <xvinh.ir@gmail.com>
2020-08-25 14:31:44 +0700

    add i love you

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-const Discord = require('discord.js');
+const Discord = require("discord.js");
 const bot = new Discord.Client();
-let cron = require('node-cron');
-require('dotenv').config();
+let cron = require("node-cron");
+require("dotenv").config();
 const token = process.env.TOKEN;
 const pexelsAPI = process.env.PEXEL;
 const unsplashAK = process.env.UNSPLASH_ACCESSKEY;
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 const yes = [
 	`Yes!`,
@@ -27,7 +27,7 @@ const yes = [
 	`Thumbs-up from me!`,
 	`Of course yes!`,
 	`Of course! Do you even need to ask?`,
-	`Duh! Everybody knows that!`,
+	`Duh! Everybody knows that!`
 ];
 const no = [
 	`No, I don't think so.`,
@@ -45,38 +45,38 @@ const no = [
 	// `Dory keeps saying 'no' but I'm not sure if she's even talking to me.`,
 	`My fortune cookie says no.`,
 	`Why don't you just roll a dice? My vote is no.`,
-	`There’s a 100% chance that I’m going say no to that one.`,
+	`There’s a 100% chance that I’m going say no to that one.`
 ];
 
-bot.on('ready', () => {
-	console.log('Naggy Nemo is alive!');
+bot.on("ready", () => {
+	console.log("Naggy Nemo is alive!");
 });
 
 const fetchDog = async () => {
-	let url = 'https://random.dog/woof.json';
+	let url = "https://random.dog/woof.json";
 	let data = await fetch(url);
 	let response = await data.json();
 	return response.url;
 };
 
 const fetchCat = async () => {
-	let url = 'https://aws.random.cat/meow';
+	let url = "https://aws.random.cat/meow";
 	let data = await fetch(url);
 	let response = await data.json();
 	return response.file;
 };
 
 const fetchMeme = async () => {
-	let url = 'https://meme-api.herokuapp.com/gimme';
+	let url = "https://meme-api.herokuapp.com/gimme";
 	let data = await fetch(url);
 	let response = await data.json();
 	return { title: response.title, url: response.url };
 };
 
 const fetchJoke = async (options) => {
-	let option = 'Any';
-	if (options.join('')) {
-		option = options.filter((str) => str).join(',');
+	let option = "Any";
+	if (options.join("")) {
+		option = options.filter((str) => str).join(",");
 	}
 	let url = `https://sv443.net/jokeapi/v2/joke/${option}?blacklistFlags=nsfw,racist,sexist`;
 	let data = await fetch(url);
@@ -87,8 +87,8 @@ const fetchJoke = async (options) => {
 const fetchDadJoke = async () => {
 	let url = `https://icanhazdadjoke.com/`;
 	let data = await fetch(url, {
-		method: 'GET',
-		headers: { Accept: 'application/json' },
+		method: "GET",
+		headers: { Accept: "application/json" }
 	});
 	let response = await data.json();
 
@@ -98,83 +98,83 @@ const fetchDadJoke = async () => {
 const fetchPxels = async (query) => {
 	let url = `https://api.pexels.com/v1/search?query=${query}&per_page=10`;
 	let data = await fetch(url, {
-		method: 'GET',
-		headers: { Authorization: pexelsAPI },
+		method: "GET",
+		headers: { Authorization: pexelsAPI }
 	});
 	let response = await data.json();
 	let photos = response.photos;
 	if (photos.length > 0) {
 		let random = Math.floor(Math.random() * photos.length);
 		return photos[random].src.landscape;
-	} else return '';
+	} else return "";
 };
 
 const fetchUnsplash = async (query) => {
 	let url = `https://api.unsplash.com/search/photos?query=${query}`;
 	let data = await fetch(url, {
-		method: 'GET',
-		headers: { Authorization: `Client-ID ${unsplashAK}` },
+		method: "GET",
+		headers: { Authorization: `Client-ID ${unsplashAK}` }
 	});
 	let response = await data.json();
 	response = response.results;
 	if (response.length > 0) {
 		let random = Math.floor(Math.random() * response.length);
 		return response[random];
-	} else return '';
+	} else return "";
 };
 
 const pickOne = (array) => {
-	let choice = array[Math.floor(Math.random() * array.length)].replace(',', '').trim();
+	let choice = array[Math.floor(Math.random() * array.length)].replace(",", "").trim();
 	return `'${choice.charAt(0).toUpperCase() + choice.slice(1)}'`;
 };
 
-bot.on('message', async (msg) => {
-	let message = msg.content.replace(/\s+/g, ' ').trim();
-	let words = message.split(' ');
+bot.on("message", async (msg) => {
+	let message = msg.content.replace(/\s+/g, " ").trim();
+	let words = message.split(" ");
 
-	if (words[0] === '<@!746413258759602246>' || words[0] === '<@746413258759602246>' || words[0] === '<@&746420675178135623>') {
+	if (words[0] === "<@!746413258759602246>" || words[0] === "<@746413258759602246>" || words[0] === "<@&746420675178135623>") {
 		// PICK ONE
-		if (words[1].toLowerCase() === 'pick' && words[2].toLowerCase() === 'one:') {
+		if (words[1].toLowerCase() === "pick" && words[2].toLowerCase() === "one:") {
 			let choices = [];
 			for (let i = 3; i < words.length; i++) {
 				choices.push(words[i]);
 			}
-			choices = choices.join(' ').split(',');
+			choices = choices.join(" ").split(",");
 			let choice = pickOne(choices);
 			return msg.reply(`${choice} sounds good!`);
 		}
 
 		// FUCK YOU
-		if (words[1].toLowerCase() === 'fu' || (words[1].toLowerCase() === 'fuck' && words[2].toLowerCase() === 'you')) {
+		if (words[1].toLowerCase() === "fu" || (words[1].toLowerCase() === "fuck" && words[2].toLowerCase() === "you")) {
 			return msg.reply(`Fuck you too, bitch!`);
 		}
 
 		// MEME
-		if (message.includes('meme')) {
+		if (message.includes("meme")) {
 			let meme = await fetchMeme();
 			msg.channel.send(meme.title, {
-				files: [meme.url],
+				files: [meme.url]
 			});
 			return;
 		}
 
 		// DAD JOKE
-		if (message.includes('dad joke')) {
+		if (message.includes("dad joke")) {
 			let dadjoke = await fetchDadJoke();
 			msg.channel.send(dadjoke);
 			return;
 		}
 		// NORMAL JOKE
-		if (message.includes('joke') || message.includes(' pun')) {
+		if (message.includes("joke") || message.includes(" pun")) {
 			let options = [];
-			if (message.includes('dark')) {
-				options.push('Dark');
+			if (message.includes("dark")) {
+				options.push("Dark");
 			}
-			if (message.includes('programming')) {
-				options.push('Programming');
+			if (message.includes("programming")) {
+				options.push("Programming");
 			}
-			if (message.includes('pun')) {
-				options.push('Pun');
+			if (message.includes("pun")) {
+				options.push("Pun");
 			}
 			let joke = await fetchJoke(options);
 			if (joke.setup) {
@@ -189,31 +189,36 @@ bot.on('message', async (msg) => {
 			return;
 		}
 
+		// I LOVE YOU
+		if (message.toLowerCase().includes("i love you") || message.toLowerCase().includes("i wuv you")) {
+			return msg.channel.send("I love me too! Now disappear from my sight!");
+		}
+
 		// YES/NO QUESTIONS
 		if (
-			words[1].toLowerCase() === 'is' ||
-			words[1].toLowerCase() === 'are' ||
-			words[1].toLowerCase() === 'will' ||
-			words[1].toLowerCase() === 'do' ||
-			words[1].toLowerCase() === 'does' ||
-			words[1].toLowerCase() === 'did' ||
-			words[1].toLowerCase() === 'am' ||
-			words[1].toLowerCase() === 'can' ||
-			words[1].toLowerCase() === 'could' ||
-			words[1].toLowerCase() === 'may' ||
-			words[1].toLowerCase() === 'might' ||
-			words[1].toLowerCase() === 'would' ||
-			words[1].toLowerCase() === 'shall' ||
-			words[1].toLowerCase() === 'should' ||
-			words[1].toLowerCase() === 'was' ||
-			words[1].toLowerCase() === 'were' ||
-			words[1].toLowerCase() === 'have' ||
-			words[1].toLowerCase() === 'has' ||
-			words[1].toLowerCase() === 'had' ||
-			((words[1].toLowerCase() === 'how' || words[1].toLowerCase() === 'what') && words[2].toLowerCase() === 'about')
+			words[1].toLowerCase() === "is" ||
+			words[1].toLowerCase() === "are" ||
+			words[1].toLowerCase() === "will" ||
+			words[1].toLowerCase() === "do" ||
+			words[1].toLowerCase() === "does" ||
+			words[1].toLowerCase() === "did" ||
+			words[1].toLowerCase() === "am" ||
+			words[1].toLowerCase() === "can" ||
+			words[1].toLowerCase() === "could" ||
+			words[1].toLowerCase() === "may" ||
+			words[1].toLowerCase() === "might" ||
+			words[1].toLowerCase() === "would" ||
+			words[1].toLowerCase() === "shall" ||
+			words[1].toLowerCase() === "should" ||
+			words[1].toLowerCase() === "was" ||
+			words[1].toLowerCase() === "were" ||
+			words[1].toLowerCase() === "have" ||
+			words[1].toLowerCase() === "has" ||
+			words[1].toLowerCase() === "had" ||
+			((words[1].toLowerCase() === "how" || words[1].toLowerCase() === "what") && words[2].toLowerCase() === "about")
 		) {
-			let answer = Math.floor(Math.random() * 2) === 0 ? 'yes' : 'no';
-			if (answer === 'yes') {
+			let answer = Math.floor(Math.random() * 2) === 0 ? "yes" : "no";
+			if (answer === "yes") {
 				return msg.channel.send(yes[Math.floor(Math.random() * yes.length)]);
 			} else {
 				return msg.channel.send(no[Math.floor(Math.random() * no.length)]);
@@ -235,8 +240,8 @@ bot.on('message', async (msg) => {
 		// }
 
 		// UNSPLASH
-		if (words[1].toLowerCase() === 'show' && words[2].toLowerCase() === 'me') {
-			let keyword = message.split(' ').slice(3).join(' ');
+		if (words[1].toLowerCase() === "show" && words[2].toLowerCase() === "me") {
+			let keyword = message.split(" ").slice(3).join(" ");
 			let photoObject = await fetchUnsplash(keyword);
 			if (!photoObject) {
 				msg.channel.send(`I can't find '${keyword}' on Unsplash. :(`);
@@ -251,18 +256,18 @@ bot.on('message', async (msg) => {
 		}
 
 		// DOG PIC
-		if (message.includes('dog')) {
+		if (message.includes("dog")) {
 			let url = await fetchDog();
 			return msg.channel.send({
-				files: [url],
+				files: [url]
 			});
 		}
 
 		// CAT PIC
-		if (message.includes('cat')) {
+		if (message.includes("cat")) {
 			let url = await fetchCat();
 			return msg.channel.send({
-				files: [url],
+				files: [url]
 			});
 		}
 
@@ -274,31 +279,31 @@ bot.on('message', async (msg) => {
 		});
 		const random = Math.floor(Math.random() * membersArray.length);
 		const person = membersArray[random].user.username;
-		if (words[1].toLowerCase() === 'who') {
+		if (words[1].toLowerCase() === "who") {
 			await msg.channel.send(`C'mon! Everyone knows it's ${person}!!`);
 			// } else if (message.startsWith('<@!746413258759602246>') && message.endsWith('?')) {
 			// 	await msg.channel.send(eightball);
-		} else msg.reply('Why are you talking to me?! Go do your guild battles!!');
+		} else msg.reply("Why are you talking to me?! Go do your guild battles!!");
 	}
 });
 
-bot.once('ready', () => {
+bot.once("ready", () => {
 	cron.schedule(
 		// min hour day month dayOfWeek
-		'0 11 1-31 1-12 0,1,3,4,5,6',
+		"0 11 1-31 1-12 0,1,3,4,5,6",
 		async () => {
 			try {
-				const channel = await bot.channels.fetch('698917300040106084');
-				if (channel) channel.send('@everyone 3 hours until GB ends!! Try your best!');
-				if (channel) channel.send('<:mongoose:740392968015577121>');
-				else console.log('Could not find channel');
+				const channel = await bot.channels.fetch("698917300040106084");
+				if (channel) channel.send("@everyone 3 hours until GB ends!! Try your best!");
+				if (channel) channel.send("<:mongoose:740392968015577121>");
+				else console.log("Could not find channel");
 			} catch (err) {
 				console.error(err);
 			}
 		},
 		{
 			scheduled: true,
-			timezone: 'Asia/Bangkok',
+			timezone: "Asia/Bangkok"
 		}
 	);
 });
